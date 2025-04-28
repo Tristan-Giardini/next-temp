b833fa3a4b52426a6402dd5590e630fe67ca7f72
Tristan Giardini <111300903+Tristan-Giardini@users.noreply.github.com>
2025-04-28 13:51:19 -0400

    Fancybox working

--- app/(site)/collections/[collection]/page.tsx
+++ app/(site)/collections/[collection]/page.tsx
@@ -1,6 +1,7 @@
 import { getCollection } from "@/sanity/sanity-utils";
 import { PortableText } from "next-sanity";
 import Image from "next/image";
+import Fancybox from "@/app/components/Fancybox";
 
 type Props = {
   params: { collection: string };
@@ -18,12 +19,36 @@ export default async function Collection({ params }: Props) {
   return (
     <div>
       {collection.name}
-      {collection.works.map((work, index) => (
+      <Fancybox
+        options={{
+          Carousel: {
+            infinite: true,
+            defaultDisplay: "flex",
+            wheel: false,
+            modal: false,
+            closeButton: true,
+          },
+        }}
+      >
+        {collection.works.map((work, index) => {
+          return (
+            <a
+              key={index}
+              data-fancybox="gallery"
+              data-caption={`<i>${work.alt}</i>, ${work.description}`}
+              href={work.asset}
+            >
+              <Image src={work.asset} alt={work.alt} width={800} height={600} />
+            </a>
+          );
+        })}
+      </Fancybox>
+      {/* {collection.works.map((work, index) => (
         <div key={index}>
           <Image src={work.asset} alt={work.alt} width={800} height={600} />
           <p dangerouslySetInnerHTML={{ __html: work.description }} />
         </div>
-      ))}
+      ))} */}
       <div>
         <PortableText value={collection.description} />
       </div>
